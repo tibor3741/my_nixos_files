4cb293d0eaeb6f6d4574dec9f3b8b377371ffca7
Tibor <36704519+tibor3741@users.noreply.github.com>
2024-01-27 21:23:27 +0100

    BIG UPDATE I KNOW IT  configuration.nix
    nix package manager uptade and automaic delete automatic  programs and sound fix it dont power off time clean tmp every boot
    and a little merge

--- configuration.nix
+++ configuration.nix
@@ -1,3 +1,7 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 { config, pkgs, ... }:
 
 {
@@ -5,19 +9,25 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-  # Bootloader. 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  # Bootloader.
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    timeout= 1;
+  };
+  
+  boot = {
+	tmp.cleanOnBoot = true; 
+  };
+  
   # Enable networking
-  networking.networkmanager.enable = true;
-
+  networking = {
+  	hostName = "nixos"; # Define your hostname.
+        networkmanager.enable = true;
+  };
+  
   # Set your time zone.
   time.timeZone = "Europe/Budapest";
 
@@ -35,11 +45,21 @@
     LC_TELEPHONE = "hu_HU.UTF-8";
     LC_TIME = "hu_HU.UTF-8";
   };
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+  services.xserver.serverFlagsSection = 
+  ''	
+      Option "StandbyTime" "0"
+      Option "BlankTime" "0"
+      Option "SuspendTime" "0"
+      Option "OffTime" "0"
+  '';
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the Budgie Desktop environment.
+  # Enable the XFCE Desktop Environment.
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
 
@@ -50,7 +70,7 @@
   };
 
   # Configure console keymap
-  console.keyMap = "hu101";
+  console.keyMap = "hu";
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -65,7 +85,7 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    jack.enable = true;
+    #jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -78,23 +98,22 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.tibor = {
     isNormalUser = true;
-    description = "tibi";
-    extraGroups = [ "networkmanager" "wheel" ];
+    description = "Tibor";
+    extraGroups = ["power" "networkmanager" "wheel" ];
     packages = with pkgs; [
       #firefox
     #  thunderbird
     ];
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
+  };1
+  
   # List packages installed in system profile. To search, run:
   # $ nix search wget
+
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-	btop nmap bat sxiv mpv geany android-tools brave vim pcmanfm vifm unrar unzip firefox qbittorrent discord betterdiscord-installer telegram-desktop 
+   # wget
+	 qbittorrent discord telegram-desktop android-tools mpv sxiv bat lshw  xfce.xfce4-pulseaudio-plugin btop nmap bat sxiv mpv brave  unrar unzip firefox
+  
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -115,14 +134,28 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+  
+  #Automatic Updates
+  system.autoUpgrade = {
+    enable = true;
+    channel = "https://nixos.org/channels/nixos-23.11";
+  };
 
+  #NixPackage Management
+  nix = {
+    settings.auto-optimise-store = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 5d ";
+    };
+  };
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "23.11"; # Did you read the comment?
 
 }
-
